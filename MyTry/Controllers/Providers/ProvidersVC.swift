c3b5c25db2e61f9a2f2fb484193a6243c4696d8e
Ibrahim <i-m-6@hotmail.com>
2022-04-16 23:30:08 +0300

    add feat in provied page

--- MyTry/Controllers/Providers/ProvidersVC.swift
+++ MyTry/Controllers/Providers/ProvidersVC.swift
@@ -7,20 +7,25 @@
 
 import UIKit
 
-class ProvidersVC: UIViewController {
+class ProvidersVC: UIViewController, UITableViewDataSource {
     
     let greetLbl = UILabel()
+    let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemYellow
+        tableView.dataSource = self
         
         view.addSubview(greetLbl)
+        view.addSubview(tableView)
         
         greetLbl.translatesAutoresizingMaskIntoConstraints = false
         greetLbl.text = "Welcome provide"
         greetLbl.font = .systemFont(ofSize: 24)
         greetLbl.textColor = .black
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             greetLbl.topAnchor.constraint(equalTo: view.topAnchor, constant: 77),
@@ -29,6 +34,19 @@ class ProvidersVC: UIViewController {
         ])
 
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ProvidersCell
+        
+        cell.textLabel?.text = "Hi every one"
+        
+        return cell
+    }
+    
     
 
 }
---
+++ ProvidersCell.swift
@@ -0,0 +1,22 @@
+//
+//  ProvidersCell.swift
+//  MyTry
+//
+//  Created by ibrahim asiri on 15/09/1443 AH.
+//
+
+import UIKit
+
+class ProvidersCell: UITableViewCell {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?){
+            super.init(style: style , reuseIdentifier: reuseIdentifier )
+
+    }
+    
+    required init?(coder: NSCoder) {
+            super.init(coder: coder)
+        }
+
+
+}
