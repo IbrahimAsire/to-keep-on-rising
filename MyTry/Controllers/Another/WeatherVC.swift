f2cbf9c83348453b146e2978e9da7165722fd77f
Ibrahim <i-m-6@hotmail.com>
2022-05-07 17:55:43 +0300

    refactor: refactoring weather page

--- MyTry/Controllers/Another/WeatherVC.swift
+++ MyTry/Controllers/Another/WeatherVC.swift
@@ -1,17 +1,28 @@
-//
-//  WeatherVC.swift
-//  MyTry
-//
-//  Created by ibrahim asiri on 05/10/1443 AH.
-//
+
 
 import UIKit
 
 class WeatherVC: UIViewController {
+    
+    let textTF: UITextField = {
+        $0.borderStyle = .bezel
+        $0.placeholder = "Type Any City!"
+        $0.textAlignment = .center
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UITextField())
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemCyan
+        
+        view.addSubview(textTF)
+        NSLayoutConstraint.activate([
+            textTF.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            textTF.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            textTF.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10)
+        
+        ])
 
     }
     
