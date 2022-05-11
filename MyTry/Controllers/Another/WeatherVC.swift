4f628781ed712e308fe71aa3c8b31a30933396e4
Ibrahim <i-m-6@hotmail.com>
2022-05-11 20:28:13 +0300

    feat: add label to country name

--- MyTry/Controllers/Another/WeatherVC.swift
+++ MyTry/Controllers/Another/WeatherVC.swift
@@ -11,16 +11,31 @@ class WeatherVC: UIViewController {
         $0.translatesAutoresizingMaskIntoConstraints = false
         return $0
     }(UITextField())
+    
+    let nameCountry = UILabel()
+    let test = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemCyan
         
         view.addSubview(textTF)
+        view.addSubview(nameCountry)
+        view.addSubview(test)
+        
+        nameCountry.translatesAutoresizingMaskIntoConstraints = false
+        nameCountry.text = "Here name of country choose"
+        test.translatesAutoresizingMaskIntoConstraints = false
+        test.text = "here the Test!"
+        
         NSLayoutConstraint.activate([
             textTF.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             textTF.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            textTF.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10)
+            textTF.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10),
+            nameCountry.topAnchor.constraint(equalTo: textTF.bottomAnchor, constant: 18),
+            nameCountry.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            test.topAnchor.constraint(equalTo: nameCountry.bottomAnchor, constant: 12),
+            test.centerXAnchor.constraint(equalTo: view.centerXAnchor),
         
         ])
 
