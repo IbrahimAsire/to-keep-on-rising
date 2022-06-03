ae7ff6fc7b1e510bb75fd4f6c524ae309bed2c9a
Ibrahim <i-m-6@hotmail.com>
2022-06-03 20:58:43 +0300

    still try solve executed else before if and repeat the moving

--- MyTry/Controllers/StartPages/CheakUser.swift
+++ MyTry/Controllers/StartPages/CheakUser.swift
@@ -5,25 +5,27 @@ import FirebaseAuth
 import FirebaseFirestore
 
 class CheakUser: UIViewController {
-        
+   
+    let db = Firestore.firestore()
+    let userVC = UsersVC()
+    let userID = Auth.auth().currentUser?.uid
+
     override func viewDidLoad() {
         super.viewDidLoad()
         selectLogin()
     }
     
     private func selectLogin() {
-        let userVC = UsersVC()
-        let userID = Auth.auth().currentUser?.uid
-        let db = Firestore.firestore()
         db.collection("Users").getDocuments { Snapshot, error in
             if error == nil {
                 let date = Snapshot!.documents
                 for ID in date {
-                    if userID == ID["UserId"] as? String {
-                        userVC.userName = ID["name"] as! String
+                    
+                    if self.userID == ID["UserId"] as? String {
+                        self.userVC.userName = ID["name"] as! String
                         print("Is a User")
                         self.navigationController?.pushViewController(
-                            userVC, animated: true)
+                            self.userVC, animated: true)
                     }else{
                         print("this is no printing")
                         self.navigationController?.pushViewController(
