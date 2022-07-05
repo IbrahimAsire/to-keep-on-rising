b52fa5e31c93c6b4cbe991f3c3a0510644e22003
Ibrahim <i-m-6@hotmail.com>
2022-07-05 15:34:15 +0300

    feat : drew and view the circle in page

--- MyTry/Controllers/Another/Circle.swift
+++ MyTry/Controllers/Another/Circle.swift
@@ -5,10 +5,31 @@ class Circle: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .secondarySystemBackground
+        
+        createCircle(startAngle: 0, endAngle: 360)
 
     }
     
+    
+    private func createSegment(startAngle: CGFloat, endAngle: CGFloat) -> UIBezierPath {
+        return UIBezierPath(arcCenter: CGPoint(x: self.view.frame.midX, y: self.view.frame.midY), radius: 150, startAngle: startAngle.toRadians(), endAngle: endAngle.toRadians(), clockwise: true)
+    }
+    
+    private func createCircle(startAngle: CGFloat, endAngle: CGFloat) {
+        let segmentPath = createSegment(startAngle: startAngle, endAngle: endAngle)
+        let segmentLayer = CAShapeLayer()
+        segmentLayer.path = segmentPath.cgPath
+        segmentLayer.lineWidth = 45
+        segmentLayer.strokeColor = UIColor.blue.cgColor
+        segmentLayer.fillColor = UIColor.clear.cgColor
 
-  
+        self.view.layer.addSublayer(segmentLayer)
+    }
+}
 
+extension CGFloat {
+    func toRadians() -> CGFloat {
+        return self * CGFloat(Double.pi) / 180.0
+    }
 }
