51c5ed53a0f3b4cc9db22018914777410ecd2627
Ibrahim <i-m-6@hotmail.com>
2022-07-25 17:54:17 +0300

    try add the content in viewContent page

--- MyTry/Controllers/Users/VCShow.swift
+++ MyTry/Controllers/Users/VCShow.swift
@@ -2,11 +2,25 @@
 import UIKit
 
 class VCShow: UIViewController {
+    
+    var viewContect = UITextView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        uiSetUp()
 
+    }
+    
+    private func uiSetUp() {
+        view.addSubview(viewContect)
+        viewContect.translatesAutoresizingMaskIntoConstraints = false
+        viewContect.backgroundColor = .purple
+        
+        NSLayoutConstraint.activate([
+            viewContect.topAnchor.constraint(equalTo: view.topAnchor, constant: 120)
+        
+        ])
     }
     
 
