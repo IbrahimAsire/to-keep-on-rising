81c0bad563f376a4fbcd45ff428dc476078ab980
Ibrahim <i-m-6@hotmail.com>
2022-10-10 19:15:11 +0300

    feat: create page to cell

---
+++ MyTry/Controllers/Providers/providerItemCell.swift
@@ -0,0 +1,16 @@
+
+import UIKit
+
+class providerItemCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+//        super.init(coder: coder)
+        
+    }
+    
+}
