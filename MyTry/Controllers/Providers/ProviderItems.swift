90220cf18017dd49c77489949c4e6c40149e8b49
Ibrahim <i-m-6@hotmail.com>
2022-10-16 17:45:12 +0300

    feat: add shape for cell in providerItem page

--- MyTry/Controllers/Providers/ProviderItems.swift
+++ MyTry/Controllers/Providers/ProviderItems.swift
@@ -42,5 +42,23 @@ class ProviderItems: UIViewController, UICollectionViewDelegate, UICollectionVie
         return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 350, height: 400)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 1, left: 0, bottom: 1, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 2
+    }
+    
     
 }
