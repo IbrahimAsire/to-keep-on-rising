03dd5f8e650bf11d736827e255496d7b4b6c0c59
Ibrahim <i-m-6@hotmail.com>
2022-10-30 08:16:56 +0300

    still try with tableView in showItem page

--- MyTry/Controllers/Providers/ShowItemVC.swift
+++ MyTry/Controllers/Providers/ShowItemVC.swift
@@ -4,9 +4,27 @@ import UIKit
 
 class ShowItemVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-
+    lazy var tableView: UITableView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.dataSource = self
+        $0.delegate = self
+        $0.register(ShowItemCell.self, forCellReuseIdentifier: "showItem")
+        $0.rowHeight = 77
+        return $0
+    }(UITableView())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+//        tableView.dataSource = self
+//        tableView.delegate = self
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 37),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            
+        
+        ])
 
         view.backgroundColor = .opaqueSeparator
     }
@@ -19,6 +37,7 @@ class ShowItemVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "showItem", for: indexPath)
         
+        cell.textLabel?.text = "Good Morning!!"
         return cell
     }
 
