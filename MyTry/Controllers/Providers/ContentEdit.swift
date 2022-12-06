be984d12feabab5b41cf53429385419961a37121
Ibrahim <i-m-6@hotmail.com>
2022-12-06 10:44:37 +0300

    style: styling the text View

--- MyTry/Controllers/Providers/ContentEdit.swift
+++ MyTry/Controllers/Providers/ContentEdit.swift
@@ -4,14 +4,22 @@ import UIKit
 
 class ContentEdit: UIViewController {
     
-    var textView: UITextView = {
-       
-        return $0
-    }(UITextView())
+    var textView = UITextView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .magenta
+        
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(textView)
+        textView.backgroundColor = .white
+        NSLayoutConstraint.activate([
+            textView.topAnchor.constraint(equalTo: view.topAnchor, constant: 66),
+            textView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -12),
+            textView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 8),
+            textView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: -8),
+        
+        ])
 
     }
     
