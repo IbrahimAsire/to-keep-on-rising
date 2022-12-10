9bfbe09fb45020cf3019607dc958e4d43ad5c340
Ibrahim <i-m-6@hotmail.com>
2022-12-10 22:34:13 +0300

    formate: styling only

--- MyTry/Controllers/Providers/ContentEdit.swift
+++ MyTry/Controllers/Providers/ContentEdit.swift
@@ -18,28 +18,23 @@ class ContentEdit: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .magenta
         
-        
+        setUpUi()
+    }
+    
+    private func setUpUi(){
         textView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(textView)
         textView.backgroundColor = .white
+        editeBtn.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(editeBtn)
+        
+        
         NSLayoutConstraint.activate([
             textView.topAnchor.constraint(equalTo: view.topAnchor, constant: 166),
             textView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -118),
             textView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 8),
             textView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: -8),
-        
-        ])
-        
-        setUpUi()
 
-
-    }
-    
-    private func setUpUi(){
-        editeBtn.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(editeBtn)
-        
-        NSLayoutConstraint.activate([
             editeBtn.topAnchor.constraint(equalTo: textView.bottomAnchor, constant: 8),
             editeBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             editeBtn.heightAnchor.constraint(equalToConstant: 38),
