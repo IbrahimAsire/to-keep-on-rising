94c6bb493d4b98b17519ccc81ffbe99bbd8310e1
Ibrahim <i-m-6@hotmail.com>
2023-04-03 20:15:35 +0300

    still the issue in firebase

--- Pods/Firebase/CoreOnly/Sources/Firebase.h
+++ Pods/Firebase/CoreOnly/Sources/Firebase.h
@@ -11,8 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-// I have issue here after update the xcode, if solve that I will delete this comment, I foundn to soluotion for this issue but still really without solution??? and now Ramadan has entered and the issue has not been resolved yet and still and I at home 9th Ramadan
+// Today 12th Ramadan and still the issue is existing
 #import <FirebaseCore/FirebaseCore.h>
 
 #if !defined(__has_include)
