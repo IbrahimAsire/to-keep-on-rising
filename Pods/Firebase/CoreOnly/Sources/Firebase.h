c79c9004fe542bd399b164d1bf62df257589f3f3
Ibrahim <i-m-6@hotmail.com>
2023-04-08 23:41:28 +0300

    fix: finally fix the issue that was in the version

--- Pods/Firebase/CoreOnly/Sources/Firebase.h
+++ Pods/Firebase/CoreOnly/Sources/Firebase.h
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// still the same issue so far 16th Ramadan!
+// still the same issue so far 17th Ramadan!
 
 #import <FirebaseCore/FirebaseCore.h>
 
