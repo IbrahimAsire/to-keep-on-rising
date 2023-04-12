7fa5924b863c42340e3eca8f5f0e5971c7a59b4f
Ibrahim <i-m-6@hotmail.com>
2023-04-12 23:13:10 +0300

    feat: add a new code

--- MyTry/Controllers/UplodFromAi.swift
+++ MyTry/Controllers/UplodFromAi.swift
@@ -4,44 +4,70 @@ import UIKit
 import FirebaseStorage
 
 class UplodFromAi: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-
-        // Get a reference to the storage service
-        let storage = Storage.storage()
-
-        // Create a reference to the file you want to upload
-        let imageRef = storage.reference().child("images/example.jpg")
-
-        // Get the local file URL
-        guard let localFile = Bundle.main.url(forResource: "example", withExtension: "jpg") else {
-            print("Error: file not found")
-            return
-        }
-
-        // Create a metadata object
-        let metadata = StorageMetadata()
-        metadata.contentType = "image/jpeg"
-
-        // Upload the file to the storage reference
-        let uploadTask = imageRef.putFile(from: localFile, metadata: metadata) { metadata, error in
+    
+    // Initialize Firebase storage reference
+    let storageRef = Storage.storage().reference()
+    
+    // Upload image to Firebase storage
+    func uploadImage(image: UIImage, imageName: String) {
+        
+        // Create a reference to the image file in Firebase storage
+        let imageRef = storageRef.child("images/\(imageName)")
+        
+        // Convert image to data format
+        guard let imageData = image.jpegData(compressionQuality: 0.75) else { return }
+        
+        // Upload image data to Firebase storage
+        let uploadTask = imageRef.putData(imageData, metadata: nil) { metadata, error in
             if let error = error {
-                // Handle error
-                print("Error: \(error.localizedDescription)")
+                print("Error uploading image: \(error.localizedDescription)")
             } else {
-                // Metadata contains file metadata such as size, content-type, and download URL.
-                let downloadURL = metadata!.bucket
-                print("Upload successful! Download URL: \(downloadURL)")
+                print("Image uploaded successfully!")
             }
         }
-
-        // Observe the upload progress
+        
+        // Track progress of image upload
         uploadTask.observe(.progress) { snapshot in
-            // A progress event occurred
             let percentComplete = 100.0 * Double(snapshot.progress!.completedUnitCount) / Double(snapshot.progress!.totalUnitCount)
             print("Upload progress: \(percentComplete)%")
         }
     }
+    
+    // Update image in Firebase storage
+    func updateImage(image: UIImage, imageName: String) {
+        
+        // Create a reference to the image file in Firebase storage
+        let imageRef = storageRef.child("images/\(imageName)")
+        
+        // Convert image to data format
+        guard let imageData = image.jpegData(compressionQuality: 0.75) else { return }
+        
+        // Update image data in Firebase storage
+        imageRef.putData(imageData, metadata: nil) { metadata, error in
+            if let error = error {
+                print("Error updating image: \(error.localizedDescription)")
+            } else {
+                print("Image updated successfully!")
+            }
+        }
+    }
+    
+    // Delete image from Firebase storage
+    func deleteImage(imageName: String) {
+        
+        // Create a reference to the image file in Firebase storage
+        let imageRef = storageRef.child("images/\(imageName)")
+        
+        // Delete image from Firebase storage
+        imageRef.delete { error in
+            if let error = error {
+                print("Error deleting image: \(error.localizedDescription)")
+            } else {
+                print("Image deleted successfully!")
+            }
+        }
+    }
 }
+
+
+//This code assumes that you have already set up Firebase in your project and have imported the necessary Firebase modules. Also, make sure that you have granted permission to access Firebase storage in your app's Info.plist file.
