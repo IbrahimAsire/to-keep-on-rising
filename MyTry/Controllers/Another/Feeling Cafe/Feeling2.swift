2a0703936a7f650b564b0ad0e319d208127cb45a
Ibrahim <i-m-6@hotmail.com>
2023-04-20 21:58:05 +0300

    feat: add pages for feeling cafe

---
+++ MyTry/Controllers/Another/Feeling Cafe/Feeling2.swift
@@ -0,0 +1,30 @@
+//
+//  Feeling2.swift
+//  MyTry
+//
+//  Created by ibrahim asiri on 29/09/1444 AH.
+//
+
+import UIKit
+
+class Feeling2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .darkGray
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MyTry/Controllers/Another/Feeling Cafe/HomeOfFeeling.swift
@@ -0,0 +1,50 @@
+//
+//  HomeOfFeeling.swift
+//  MyTry
+//
+//  Created by ibrahim asiri on 29/09/1444 AH.
+//
+
+import UIKit
+
+class HomeOfFeeling: UIViewController {
+    
+    let image1 = UIImageView()
+    let page2Btn = UIButton()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        view.backgroundColor = .init(red: 1, green: 0.6235294118, blue: 0.03921568627, alpha: 0.7595131803)
+        view.backgroundColor = .lightText
+
+
+        image1.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(image1)
+        image1.image = UIImage(named: "f1")
+        
+        page2Btn.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(page2Btn)
+        page2Btn.addTarget(self, action: #selector(goPreesTpd), for: .touchUpInside)
+
+        NSLayoutConstraint.activate([
+            image1.topAnchor.constraint(equalTo: view.topAnchor, constant: 86),
+            image1.widthAnchor.constraint(equalTo: view.widthAnchor),
+            image1.heightAnchor.constraint(equalToConstant: 600),
+            
+            page2Btn.topAnchor.constraint(equalTo: view.topAnchor, constant: 86),
+            page2Btn.widthAnchor.constraint(equalTo: view.widthAnchor),
+            page2Btn.heightAnchor.constraint(equalToConstant: 600),
+
+
+        
+        ])
+        
+
+    }
+   
+    @objc func goPreesTpd(){
+        navigationController?.pushViewController(Feeling2(), animated: true)
+    }
+
+}
